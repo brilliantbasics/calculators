1a45795aa802f9703c24f31367fa01309ac22440
Miguel Diaz <miguel.diaz@codescrum.com>
2013-08-30 21:35:07 -0500

    add js to basic water specs and correct issue with simulated slider spec

--- spec/features/basic/water/report_walkthrough_spec.rb
+++ spec/features/basic/water/report_walkthrough_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe "Basic Tool Walkthrough" do
-  context 'when a guest user uses the calculator', :focus do
+  context 'when a guest user uses the calculator' do
 
-    it 'starts the calculator' do
+    it 'starts the calculator', :js do
       start_calculator :water, :basic
       current_path.should == water_basic_path
     end
@@ -16,19 +16,37 @@ describe "Basic Tool Walkthrough" do
       basic_water[:country].should == "CO"
     end
 
-    it 'selects the main water supply technology' do
+    it 'selects the main water supply technology', :js do
       visit '/water_basic/water'
-      choose 'mixedPipe'
+      pick_box 'boreHold_03'
       click_button t('buttons.next')
       current_path.should == '/water_basic/population'
-      ap session
-      basic_water[:water].should == 1
+      basic_water[:water].should == 3
+    end
+
+    it 'selects the population', :js  do
+      visit '/water_basic/population'
+
+      slider_handle = page.find('.ui-slider-handle')
+      info_tab = page.find('.tab')
+      slider_handle.drag_to(info_tab)
+
+      click_button t('buttons.next')
+      current_path.should == '/water_basic/capital'
+      basic_water[:population].should == 1000000
     end
 
     # a more semantic approach to naming the session
     # could be named "report" too
     def basic_water
       basic_water_session
+    end
+
+    # click one of the typical boxes in the UI when selecting icons
+    # @param label_name [String] the actual "class" for the label icon image
+    # e.g. boreHold_03, waterTool << css class names
+    def pick_box(label_name)
+      find("label.#{label_name}").click
     end
 
   end
